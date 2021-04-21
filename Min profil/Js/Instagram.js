30b4326e0242a4d99bcd33e856cb9af9ab922947
rydazz <55440799+rydazz@users.noreply.github.com>
2021-04-21 17:25:06 +0200

    Delete Min profil directory

--- Min profil/Js/Instagram.js
+++
@@ -1,6 +0,0 @@
-﻿function inst() {
-
-    //window.location.replace("https://www.linkedin.com/in/daniel-rydqvist-128577204?trk=people-guest_people_search-card");
-    //window.location.href = "https://www.instagram.com/rydisen/";
-    window.open("https://www.instagram.com/rydisen/", "_blank");
-}
--- Min profil/Js/LaddaCV.js
+++
@@ -1,6 +0,0 @@
-﻿$(document).ready(function () {
-    $("#link").click(function (e) {
-        e.preventDefault();
-        window.open("https://drive.google.com/file/d/1DP0snJrIsiWl-utJabVccEgGtBEJffLr/view?usp=sharing", "_blank");
-    });
-});
